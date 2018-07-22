3046c2939a6f5f068b974cbbb5c971cccca130ca
Tobias Bester <u14041368@tuks.co.za>
2018-07-22 18:37:26 +0200

    Created new Ionic project

---
+++ minibusinessmanager/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ClientsPage } from '../pages/clients/clients';
+import { OrdersPage } from './../pages/orders/orders';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    ClientsPage,
+    OrdersPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    ClientsPage,
+    OrdersPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
