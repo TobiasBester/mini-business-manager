f54ebf57ba2c70f6b0bb1e46e07e2370c0588fe7
Tobias Bester <u14041368@tuks.co.za>
2018-08-07 23:04:19 +0200

    Finished Template for Client Page. Ionic 4 is weird

--- minibusinessmanager/src/pages/clients/clients.ts
+++ minibusinessmanager/src/pages/clients/clients.ts
@@ -1,6 +1,6 @@
 import { AddClientPage } from './../add-client/add-client';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, IonicPage } from 'ionic-angular';
 
 /**
  * Generated class for the ClientsPage page.
@@ -8,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-clients',
